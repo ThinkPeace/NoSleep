6b993d7edf6141db2bfd18c666be47ef4c31e06a
ThinkPeace <1@icloud.com>
2026-01-19 17:44:17 +0800

    chore: bump homebrew formula to v0.1.3

--- homebrew/nosleep.rb
+++ homebrew/nosleep.rb
@@ -1,12 +1,12 @@
 class Nosleep < Formula
   desc "macOS no-sleep CLI wrapper around caffeinate"
   homepage "https://github.com/ThinkPeace/NoSleep"
-  url "https://github.com/ThinkPeace/NoSleep/releases/download/vX.Y.Z/nosleep-X.Y.Z"
-  sha256 "REPLACE_WITH_SHA256"
-  version "X.Y.Z"
+  url "https://github.com/ThinkPeace/NoSleep/releases/download/v0.1.3/nosleep-0.1.3"
+  sha256 "05aa5fcc3634e52088b8c4b15b033711978ef199129bda2c4fc62d7b5d04fcdb"
+  version "0.1.3"
 
   def install
-    bin.install "nosleep-X.Y.Z" => "nosleep"
+    bin.install "nosleep-0.1.3" => "nosleep"
   end
 
   test do
